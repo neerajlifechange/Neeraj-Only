2f710af701856f798ac6c1a58f0439777a2f93a7
neerajlifechange <144465632+neerajlifechange@users.noreply.github.com>
2023-12-15 17:40:42 +0530

    Add files via upload

---
+++ Original.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Clone the GitHub repository
+git clone https://github.com/neerajlifechange/Neeraj-Only.git
+
+# Navigate to the repository directory
+cd Neeraj-Only
+
+# Run the Python script
+python run.py
